36de1369e0cfe0514c1146c2087d48bd8cabba7c
Pranshuk29 <145792354+Pranshuk29@users.noreply.github.com>
2024-07-30 20:30:12 +0530

    Add files via upload

--- P1_simply.py
+++ P1_simply.py
@@ -2,16 +2,16 @@ import aiml
 
 kernel=aiml.Kernel() #this is the brain library behind AI chatbots 
 kernel.learn("P1_std_startup.xml")
-# kernel.respond("LOAD AIML B")
-print("Welcome to AI Chatbot!")
+kernel.respond("LOAD AIML B")
+print("Welcome to Your Chatbot!")
 
 while True:
     input_ques=input("User: ")
-    if input_ques.lower() == 'exit':
-        print("Adios Amigo!") #Spanish for Goodbye, my Friend
-        break
+    # if input_ques.lower() == 'exit': handled in aiml file
+    #     print("Adios Amigo!") #Spanish for Goodbye, my Friend
+    #     break
     response=kernel.respond(input_ques)
-    if response=='':
+    if "WARNING" in response:
         response="I'm having trouble understanding. Could you please rephrase?"
         
     print("Bot: ",response)
