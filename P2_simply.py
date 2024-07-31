9d6bfa4a12e66a6365f008f9a7f3a4cb99569963
Pranshuk29 <145792354+Pranshuk29@users.noreply.github.com>
2024-07-31 15:56:45 +0530

    Add files via upload

--- P2_simply.py
+++ P2_simply.py
@@ -1,17 +1,17 @@
 import aiml
 
 kernel=aiml.Kernel() #this is the brain library behind AI chatbots 
-kernel.learn("P1_std_startup.xml")
+kernel.learn("P2_std_startup.xml")
 kernel.respond("LOAD AIML B")
-print("Welcome to Your Chatbot!")
+print("Welcome to Your College Admission Chatbot!")
 
 while True:
     input_ques=input("User: ")
-    if input_ques.lower() == 'exit':
+    if input_ques.lower() == 'exit': 
         print("Adios Amigo!") #Spanish for Goodbye, my Friend
         break
     response=kernel.respond(input_ques)
     if "WARNING" in response:
-        response="I'm having trouble understanding. Could you please rephrase?"
+        response="I'm having trouble understanding. Could you please rephrase or ask something more relevant to college admissions?"
         
     print("Bot: ",response)
